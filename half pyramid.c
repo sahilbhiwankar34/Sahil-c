6b2050c3456f5d1e32b6016d70f1db2bd4f989dd
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2025-12-23 20:03:45 +0530

    Add files via upload

---
+++ half pyramid.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() {
+	int i,j,row;
+	printf("Enter the rows: ");
+	scanf("%d",&row);
+	for(i=1;i<=row;i++){
+		for(j=1;j<=i;j++){
+			printf("+");
+		}
+		printf("\n");
+	}
+}
