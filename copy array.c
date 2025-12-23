d737f8df1c8cc845da4d150392590ac1487fbaf8
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2025-12-23 20:06:23 +0530

    Add files via upload

--- copy array.c
+++ copy array.c
@@ -1,23 +1,38 @@
 #include<stdio.h>
 void main() {
-	int i,arr[10],arr1[10],n;
-	printf("Enter the count elements: ");
+	int a[15],b[15],n,i,*p1,*p2;
+	
+	printf("Enter the count of array\n");
 	scanf("%d",&n);
+	
+	p1=a;
+	p2=b;
+	
 	printf("Enter the elements of array\n");
-	for(i=0;i<n;i++){
-		printf("Elements-%d = %d",i+1);
-		scanf("%d",&arr[i]);
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",p1+i);
+	}
+	
+	printf("\nThe array elements is\n");
+	
+	p1=a;
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",*p1);
+		p1++;
+	}
+	
+	printf("\n After copying the second array is\n");
+	p1=a;
+	for(i=0;i<n;i++)
+	{
+		*p2=*p1;
+		printf("%d\n",*p2);
+		p1++;
+		p2++;
 		
 	}
-	for(i=0;i<n;i++){
-		arr1[i]=arr[i];
-	}
-	printf("The elements of first array is\n");
-	for(i=0;i<n;i++){
-		printf("%d",arr[i]);
-	}
-	printf("After copying the elements of first srray in second array is\n");
-	for(i=0;i<n;i++){
-		printf("%d",arr1[i]);
-	}
+	
 }
