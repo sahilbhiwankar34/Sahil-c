87363ce712f21df000024b7b5a7c1d100420bf97
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2025-12-23 19:49:18 +0530

    Add files via upload

---
+++ ascending and descending.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+	int i=0,j=10;
+	printf("THE ascending order is :\n");
+	for(i=0;i<=10;i++)
+	{
+		printf("%d\n",i);
+	}
+	printf("The descending order is :\n");
+	for(j=10;j>=0;j--)
+	{
+		printf("%d\n",j);
+	}
+	
+}
