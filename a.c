e03b1a75f40549cd8c2c8977d874869d19ccd569
sahilbhiwankar34 <sahilbhiwankar34@gmail.com>
2025-12-23 19:48:32 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the number:");
+	scanf("%d%d",&a,&b);
+	
+	if((a>b) && a==0)
+	printf("%d IS GREATER",a);
+	else
+	printf("wrong input");
+	
+	
+	
+	}
